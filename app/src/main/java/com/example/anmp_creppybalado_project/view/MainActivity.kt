76c1be16b327c97299b908e868ac59a95c2a49fd
DedeJP <s160420140@student.ubaya.ac.id>
2024-09-05 10:52:25 +0700

    create_newfile

---
+++ app/src/main/java/com/example/anmp_creppybalado_project/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.anmp_creppybalado_project.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.anmp_creppybalado_project.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
