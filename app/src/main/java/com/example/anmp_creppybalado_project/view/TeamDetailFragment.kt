91fdb0bba71f3ba35feb14a447351221eb8673dd
Rudi Novianto <s160420086@student.ubaya.ac.id>
2024-10-20 12:42:55 +0700

    Team Detail

--- app/src/main/java/com/example/anmp_creppybalado_project/view/TeamDetailFragment.kt
+++ app/src/main/java/com/example/anmp_creppybalado_project/view/TeamDetailFragment.kt
@@ -1,54 +1,34 @@
 package com.example.anmp_creppybalado_project.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.anmp_creppybalado_project.R
 import com.example.anmp_creppybalado_project.databinding.FragmentTeamBinding
+import com.example.anmp_creppybalado_project.databinding.FragmentTeamDetailBinding
 import com.example.anmp_creppybalado_project.viewmodel.ListTeamModel
 import com.squareup.picasso.Picasso
 
-class TeamFragment : Fragment() {
-    private lateinit var binding: FragmentTeamBinding
+
+class TeamDetailFragment : Fragment() {
+    private lateinit var binding: FragmentTeamDetailBinding
     private lateinit var viewModel: ListTeamModel
-    private var imageUrl: String? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentTeamBinding.inflate(inflater, container, false)
-
+        binding = FragmentTeamDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val imageUrl = arguments?.let { TeamFragmentArgs.fromBundle(it).url }
+        val imageUrl = arguments?.let { TeamDetailFragmentArgs.fromBundle(it).url }
 
         imageUrl?.let { url ->
             Picasso.get().load(url).into(binding.imgGameTeam)
-        }
-        binding.btnTeamA.setOnClickListener {
-            navigateToDetail(imageUrl)
-        }
-
-        binding.btnTeamB.setOnClickListener {
-            navigateToDetail(imageUrl)
-        }
-
-        binding.btnTeamC.setOnClickListener {
-            navigateToDetail(imageUrl)
-        }
-
-    }
-
-    private fun navigateToDetail(url: String?) {
-        url?.let {
-            val action = TeamFragmentDirections.actionTeamDetailFragment(url = it)
-            findNavController().navigate(action)
         }
     }
 }
